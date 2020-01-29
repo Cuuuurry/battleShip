8e6ef577819ae843f3a6469546c306163db7df4c
Chenghan Sun <sunchenghan1995@gmail.com>
2020-01-28 16:30:58 -0800

    re-init all code

--- TicTacToe/player.py
+++ TicTacToe/player.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-class Player(object):
-    def __init__(self) -> None:
-        self.name = None
-        self.board = None
+"""
+This File:
+    
+"""
 
-    def __str__(self) -> str:
-        return self.name
-
-    def
-=======
 
 class Player(object):
     def __init__(self) -> None:
@@ -18,4 +12,3 @@ class Player(object):
     def __str__(self) -> str:
         return self.name
 
->>>>>>> 33b8268ba5f79c62308a0ca469c12374001c80d3
