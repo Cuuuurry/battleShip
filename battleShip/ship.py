c02295da4d407992f85f073c28cda12f78708223
Chenghan Sun <sunchenghan1995@gmail.com>
2020-01-28 19:55:22 -0800

    new main&ship

--- battleShip/ship.py
+++ battleShip/ship.py
@@ -1,42 +1,17 @@
 """
 This File:
     Ship information
+        Count the fire
 """
 
 import typing
 
-class ship(object):
-    def __init__(self, shipNum: int):
-        self.shipNum = shipNum
-        self.ship_dict = {}
 
-    def ship_num_setup(self):
-        for i in range(self.shipNum):
-            newitem = input("Please enter the new ship name")
-            if newitem in self.ship_dict:
-                raise Exception("The category of ship have existed")
-                erase = input("The category of ship have existed. \n"
-                              "Do you want to redefine the number of item? \n  "
-                              "Enter True or False.")
-                if erase == "True" or "true":
-                    newnum = input("Please enter the new numbers of ships")
-            else:
-                newnum = input("Please enter the new numbers of ships")
-            self.ship_dict[newitem] = {}
-            self.ship_dict[newitem]["num"] = int(newnum)
+class Ship(object):
+    def __init__(self, board_size, ship_size_dict) -> None:
+        self.board_size = board_size
+        self.ship_size_dict = ship_size_dict
 
-    def ship_size_setup(self):
-        for i in range(self.shipNum):
-            newitem = input("Please enter the new ship name")
-            if newitem in self.ship_dict:
-                raise Exception("The category of ship have existed")
-                erase = input("The category of ship have existed. \n"
-                              "Do you want to redefine the number of item? \n  "
-                              "Enter True or False.")
-                if erase == "True" or "true":
-                    newnum = input("Please enter the new numbers of ships")
-            else:
-                newnum = input("Please enter the new numbers of ships")
-            self.ship_dict[newitem] = {}
-            self.ship_dict[newitem]["num"] = int(newnum)
+
+
 
