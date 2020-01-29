20f8c28e7d13a4aa22ab94543e7af209bcbda2ba
Cuuuurry <scywu@ucdavis.com>
2020-01-28 21:41:53 -0800

    new board

--- battleShip/board.py
+++ battleShip/board.py
@@ -12,32 +12,37 @@ class Board(object):
         self.num_cols = num_cols
         self.is_scan = is_scan
         if self.is_scan:
-            self.scan_char = "(" + blank_char + ")"
+            self.scan_char = "0"
         self.board = [[blank_char for col in range(num_cols)] for row in range(num_rows)]
         self.blank_char = blank_char
 
     def __str__(self):
-        sep = ' ' * max([len(str(self.num_rows)), len(str(self.num_cols))]) + '\n'
-        rep = sep + sep.join((str(i) for i in range(self.num_cols)))
+        max_digit_num = max([len(str(self.num_rows)), len(str(self.num_cols))])
+        sep = ' ' * max_digit_num #+ '\n'
+        rep = sep
+        for i in range(self.num_cols):
+            rep += ' ' + ' ' * (max_digit_num - len(str(i))) + str(i)
+        rep += "\n"
         for row_index, row in enumerate(self):
-            rep += str(row_index) + sep + sep.join(row) + '\n'
+            num_of_space = max_digit_num - len(str(row_index))
+            rep += ' ' * num_of_space + str(row_index) + sep + sep.join(row) + '\n'
         return rep
 
     def __iter__(self) -> Iterator[List[str]]:
         return iter(self.board)
 
+    def __getitem__(self, row: int, col: int) -> List[str]:
+        return self.board[row][col]
 
-    def scan_or_place(self):
-       pass
-
-    #def
+    def board_record(self, row, col, record_stuff):
+        self.board[row][col] = record_stuff
 
 
 
 
 
 if __name__ == "__main__":
-    board_case = Board(3,3)
+    board_case = Board(13,13)
     print(board_case)
 
 
