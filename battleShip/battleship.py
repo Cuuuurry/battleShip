058b43a3adb24ee9ab1751ecd7f4fce30f14ff67
Chenghan Sun <sunchenghan1995@gmail.com>
2020-01-29 19:42:47 -0800

    update func battleship

--- battleShip/battleship.py
+++ battleShip/battleship.py
@@ -17,12 +17,103 @@ class BattleShip(object):
         Two players
         Four boards: two initial boards (static), two scan boards (dynamic)
     """
-    def __init__(self, num_rows: int, num_cols: int, is_scan: bool = False, miss_char: str = "X",
-                 aim_char: str = "O") -> None:
+    def __init__(self, ship_size, ) -> None:
+        self.ship_size = ship_size
         self.blank_char = blank_char
         self.board = Board(num_rows, num_cols, is_scan)
         self.players = [Player() for _ in range(2)]
         self.cur_player_turn = 0
+
+    def load_ships(self, size, x, y, orientation):
+        '''Place ship of size, size, starting at position (x,y) and oriented
+        vertically (oreitnation = 0) or horizontally (orientation = 1).'''
+        self.check_collisions(size, x, y, orientation)
+        # Checks to make sure the ship doesn't lie outside the board and that
+        # no ships have been placed on those spots.
+        if not orientation:
+            for x in range(x, x + size):
+                self.board[(x,y)] = size
+        elif orientation:
+            for y in range(y, y + size):
+                self.board[(x,y)] = size
+
+    def check_collisions(self, size, x, y, orientation):
+        '''Checks to make sure the ship doesn't lie outside the board and that
+        no ships have been placed on those spots.'''
+        if not orientation:
+            if self.size < (x + size) or self.size < y:
+                raise OutofboardError()
+            for x in range(x, x + size):
+                if self.board.get((x, y)) != '.':
+                    raise ShipAlreadyThere()
+        elif orientation:
+            if self.size < (y + size) or self.size < x:
+                raise OutofboardError()
+            for y in range(y, y + size):
+                if self.board.get((x, y)) != '.':
+                    raise ShipAlreadyThere()
+
+    def fire(self):
+        if not self.human:
+            # checks to see if the current player should be a computer.
+            x = random.randint(0, self.size)
+            y = random.randint(0, self.size)
+        else:
+            x = raw_input('What is the x-coordinate you wish to fire on? ')
+            y = raw_input('What is the y-coordinate you wish to fire on? ')
+        try:
+            x, y = int(x), int(y)
+            # verifies that x and y are valid integers.
+        except Exception:
+            self.fire()
+        self.fire_helper(x, y)
+
+    def fire_helper(self, x, y):
+        '''Fires at coordinates (x,y) on opponent'''
+
+        if x > self.size or y > self.size:
+            # Checks to make sure that x and y and in the scope of the board.
+            print
+            'Out of bounds'
+            self.fire()
+        elif self.opponents_board.get((x, y)) != '?':
+            # Checks if the current spot has been chosen.
+            print
+            'That coordinate has been fired already'
+            self.fire()
+        elif (self.opponent.board.get((x, y)) == '.' or
+              self.opponent.board.get((x, y)) == 'x'):
+            # The player has hit an s and missed.
+            print
+            'Target missed'
+            self.opponents_board[(x, y)] = 'x'
+            self.opponent.board[(x, y)] = 'x'
+        else:
+            # A player's ship has been hit! Mark it on the board.
+            ship_name_list = ['Destroyer', 'Frigate', 'Battleship', 'Carrier']
+
+            print("Hit enemy's " + \
+                  ship_name_list[self.opponent.board.get((x, y)) - 2])
+            self.score -= 1
+            self.opponents_board[(x, y)] = \
+                self.opponent.board.get((x, y))
+            self.opponent.board[(x, y)] = 'x'
+
+    def turn(self):
+        raw_input(self.name + ''''s turn''' + ' push enter to continue')
+        if not self.human and self.opponent.human:
+            pass
+        else:
+            print
+            'Your board:'
+            self.print_map(True)
+            print
+            '''Your view of your opponent's board:'''
+            self.print_map(False)
+            # Print your view of the opponents map.
+        self.fire()
+        for n in range(20):
+            print('')
 
     def play(self) -> None:
         """
