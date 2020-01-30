45900cb89450721c25ee46992aae4bfdb0c958bf
Chenghan Sun <sunchenghan1995@gmail.com>
2020-01-29 19:22:43 -0800

    new ver battleship set-up

--- battleShip/battleship.py
+++ battleShip/battleship.py
@@ -1,21 +1,36 @@
 """
 This File:
-    Game process
+    Class battleShip for game process
+
 """
 
 from typing import Iterable, TypeVar
-from .board import Board
-from .player import Player
+from board import Board
+from player import Player
+from ship import Ship
+print("Successfully import classes")
 
 
-class battleShip(object):
-    def __init__(self, dimensions: int, blank_char: str = "*") -> None:
+class BattleShip(object):
+    """
+    Summary:
+        Two players
+        Four boards: two initial boards (static), two scan boards (dynamic)
+    """
+    def __init__(self, num_rows: int, num_cols: int, is_scan: bool = False, miss_char: str = "X",
+                 aim_char: str = "O") -> None:
         self.blank_char = blank_char
-        self.board = Board(dimensions,dimensions,blank_char)
+        self.board = Board(num_rows, num_cols, is_scan)
         self.players = [Player() for _ in range(2)]
-        self._cur_player_turn = 0
+        self.cur_player_turn = 0
 
     def play(self) -> None:
+        """
+        Summary:
+                Main loop of the game
+
+        :return:
+        """
         while not self.is_game_over():
             self.display_game_state()
             cur_player = self.get_cur_player()
@@ -42,8 +57,8 @@ class battleShip(object):
         for row in self.board:
             if all_same(row):
                 return True
-
-        return False
+            else:
+                return False
 
         # for i in range(self.board.rows):
         #     if self.board.row_all_same(self.board[i])
@@ -60,16 +75,13 @@ class battleShip(object):
 
 
 
-
-
-
-
-    def change_turn(self) -> None:
+    def change_turn(self):
         self._cur_player_turn = (self._cur_player_turn + 1) % 2
         if self._cur_player_turn == 0:
             self._cur_player_turn = 1
         else:
             self._cur_player_turn = 0
 
-    def get_cur_player(self) -> "Player":
+    def get_cur_player(self):
+
         return self.players[self._cur_player_turn]
