1728acaf4aedd854062e6f8091903743358dd475
Cuuuurry <scywu@ucdavis.edu>
2020-02-12 18:23:46 -0800

    conflicts fixed

---
+++ battleShip/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This File:
+    Main script for Battleship game execution
+"""
+
+from sys import argv
+from battleship import BattleShip
+import numpy as np
+
+if __name__ == '__main__':
+    # get line information in the configs file
+    with open(argv[2], "r") as f2:
+        seed_number = f2.readline()
+    np.random.seed(int(seed_number))
+    ship_size_dict = {}
+    board_size = []
+    with open(argv[1], "r") as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                board_size = [int(size) for size in line.split()]
+            else:
+                ship_size_dict[line.split()[0]] = line.split()[1]
+
+    # test = BattleShip(board_size[0], board_size[1], ship_size_dict)
+    # test.play()
